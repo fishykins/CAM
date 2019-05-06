2b9120cfccc4f033d7edf813cca0fbe4d8849cc8
Fishy <caspar.m.green@gmail.com>
2019-05-06 10:39:19 +0100

    First commit

---
+++ CAM/Program.cs
@@ -0,0 +1,48 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        #region Variables
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+        public Program()
+        {
+            
+        }
+
+        public void Save()
+        {
+            
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            
+        }
+        #endregion
+    }
+}
